9a69eb79cba15dd7da1e4826fb04bacf383a161d
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2024-01-31 19:43:45 +0300

    done with my second kotlin application

--- app/src/main/java/com/example/kotlinsecondapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsecondapplication/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
 
+
+import android.annotation.SuppressLint
+import android.widget.RadioButton
+import com.example.kotlinsecondapplication.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +16,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.button.setOnClickListener{
+            val checkedRadioButtonId = binding.BurgerKind.checkedRadioButtonId
+            val burgerKind = findViewById<RadioButton>(checkedRadioButtonId)
+
+            val chesse = binding.Cheese.isChecked
+            val jalapeno = binding.Jalapeno.isChecked
+            val tomatoes = binding.Tomatoes.isChecked
+
+            val order = "you ordered ${burgerKind.text} burger with "+ if (chesse){"cheese "}else{""} +if (jalapeno){"jalapeno "}else{""} + if (tomatoes){"tomatoes "}else{""}
+
+            binding.textView4.text = order
+        }
     }
 }
